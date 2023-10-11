26483e9144cdfb8ca7c95eecd8ec4f75e28d68d6
sahusuman9 <suman.sahu.981@gmail.com>
2023-10-11 23:02:08 +0530

    commit

--- app.js
+++ app.js
@@ -33,3 +33,16 @@ app.listen(port,host, (err) => {
     console.log(`Server is running at port: ${port} with hostname: ${host}`)
     console.log(`http://${host}:${port}`)
 })
+
+
+
+// The selected text is a script written in Node.js using the Express.js framework. Here's what it does:
+
+// - **Imports modules**: It imports the `express`, `dotenv`, `morgan`, and `path` modules.
+// - **Initializes Express**: It creates an instance of an Express application.
+// - **Uses middleware**: It uses the `morgan` middleware for logging HTTP requests.
+// - **Configures dotenv**: It configures `dotenv` to load environment variables from a `.env` file.
+// - **Sets up routes**: It sets up routes for the root URL ("/") and "/about", "/contact", and "/login" endpoints. Each route sends an HTML file as a response.
+// - **Starts the server**: It starts the server on a port and host specified in the environment variables. It logs a message when the server is running.
+
+// This script is a basic setup for a web server that can serve static HTML files. The server logs all incoming HTTP requests, which can be useful for debugging. The use of environment variables for configuration makes the script flexible and secure. The server serves different HTML files depending on the route accessed by the client. This is a common pattern in web development, where different URLs correspond to different pages on a website. The script also handles errors that might occur when starting the server.
